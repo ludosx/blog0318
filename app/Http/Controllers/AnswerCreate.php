62f80add66971e327a554b4a41dcbc2f20785695
Your Name <you@example.com>
2023-03-19 16:58:27 -0500

    answers by each post

--- app/Http/Controllers/AnswerCreate.php
+++ app/Http/Controllers/AnswerCreate.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Post;
+use App\Models\Answer;
 
 
 class AnswerCreate extends Controller
@@ -12,6 +13,10 @@ class AnswerCreate extends Controller
 
     public function __invoke(Request $request)
     {
-        return view('answer', ['post' => Post::find($request->post_id)]);
+        return view('answer', [
+            'post' => Post::find($request->post_id),
+            'answers' => Answer::where('post_id', $request->post_id)->get()
+            //'answers' => Answer::all()
+        ]);
     }
 }
--- resources/views/answer.blade.php
+++ resources/views/answer.blade.php
@@ -1,5 +1,11 @@
+@inject('userClass', 'App\Models\User')
+
 @extends('layouts.app')
 @section('content')
+
+
+
+    <div class="block mx-auto my-12 p-8 bg-white w-1/3 border border-gray-200 rounded-lg shadow-lg">
 
     <p>{{ $post->title }}</p>
     <p>{{ $post->text_post}}</p>
@@ -12,5 +18,19 @@
         <br>
         <input type="submit" value="answer">
         <br>
+        <br>
+
     </form>
+
+    @foreach($answers as $answer)
+    <div class="block mx-auto my-12 p-8 bg-white w-1/3 border border-gray-200 rounded-lg shadow-lg">
+        <p>{{ $userClass->find($answer->user_id)->name }}</p>
+        <p>{{ $answer->answer }}</p>
+        <br>
+    </div>
+    @endforeach    
+
+    </div>
+
+
 @endsection
