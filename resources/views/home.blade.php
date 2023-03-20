073946bc4a500544bc1e5aa4f5970bf71a34b746
Your Name <you@example.com>
2023-03-20 07:58:32 -0500

    showing posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,12 +15,17 @@
 
     <h1 class="text-3xl text-center font-bold">{{ $post->title }}</h1>
 
-    <form class="mt-4" method="post" action="{{ route('answer.create') }}">
-    @csrf
-
+    @auth
+        <form class="mt-4" method="post" action="{{ route('answer.create') }}">
+        @csrf
+    @endauth
+    @guest
+        <form class="mt-4" method="get" action="{{ route('session.create') }}">  
+    @endguest
     <p>Author: {{ ($userClass->find($post->user_id))->name }}</p>
     <p>{{ $post->text_post }}</p>
     <input type="hidden" name="post_id" value="{{ $post->id }}">
+
     <button type="submit" class="rounded-md bg-indigo-500 w-full text-lg text-white font-semibold p-2 my-3 hover:bg-indigo-600">Show</button>
 
     </form>
